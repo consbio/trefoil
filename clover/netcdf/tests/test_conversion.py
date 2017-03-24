0abe08a6186f1b34410031f2bd2c243b2feafd8d
Brendan Ward <bcward@consbio.org>
2017-03-23 22:04:01 -0700

    Added first pass of netcdf to raster conversion

---
+++ clover/netcdf/tests/test_conversion.py
@@ -0,0 +1,39 @@
+import os
+from netCDF4 import Dataset
+from pyproj import Proj
+import numpy
+import rasterio
+from clover.netcdf.crs import PROJ4_GEOGRAPHIC
+from clover.netcdf.conversion import netcdf_to_raster
+
+
+# TODO: need 3d test file
+# TODO: need better test fixtures to fabricate data
+
+def test_netcdf_to_raster(tmpdir):
+    outfilename = str(tmpdir.join('test.tif'))
+
+    dataset = Dataset('clover/test_data/tmin.nc')
+    varname = 'tmin'
+    y_name, x_name = dataset.variables[varname].dimensions[:2]
+
+    netcdf_to_raster(
+        dataset,
+        varname,
+        outfilename,
+        projection=Proj(PROJ4_GEOGRAPHIC)
+    )
+
+    assert os.path.exists(outfilename)
+
+    variable = dataset.variables[varname]
+    height, width = variable.shape[:2]
+    data = variable[:]
+
+    with rasterio.open(outfilename, 'r') as src:
+        assert src.count == 1
+        assert src.width == width
+        assert src.height == height
+        assert src.crs.to_string() == '+init=epsg:4326'
+        assert src.dtypes[0] == data.dtype
+        assert numpy.array_equal(data, src.read_band(1))
---
+++ clover/utilities/conversion.py
@@ -0,0 +1,27 @@
+import rasterio
+from affine import Affine
+
+
+def array_to_raster(data, outfilename=None, format='GTiff', affine=Affine.identity(), projection=None):
+    """
+    Only GTiff driver supported at present.
+
+    Will implicitly overwrite existing output.
+    prj must be a pyproj.Proj object
+    """
+
+    if format != 'GTiff':
+        raise NotImplementedError('Formats besides GTiff not yet supported')
+
+    meta = {
+        'width': data.shape[1],
+        'height': data.shape[0],
+        'dtype': data.dtype,
+        'transform': affine,
+        'count': 1,
+        'crs': projection.srs
+    }
+
+    with rasterio.Env():
+        with rasterio.open(outfilename, 'w', driver=format, **meta) as out:
+            out.write(data, 1)
