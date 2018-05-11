1b6d13b88be9cfc3327a22e86745fa3fbb015762
Nik Molnar <nik.molnar@consbio.org>
2018-05-10 20:37:40 -0700

    Apply to all variables by default

--- trefoil/cli/crs.py
+++ trefoil/cli/crs.py
@@ -6,25 +6,20 @@ from trefoil.netcdf import crs
 from trefoil.cli import cli
 
 
-@cli.command(short_help='Set spatial reference information for a variable in an existing dataset')
+@cli.command(short_help='Set spatial reference information for variables in an existing dataset')
 @click.argument('filename', type=click.Path(exists=True))
 @click.argument('proj4')
-@click.argument('variables', required=False)
-@click.option('--all', 'all_variables', is_flag=True, default=False, help='Set CRS for all variables in the dataset')
-def set_crs(filename, proj4, variables, all_variables):
-    if not variables and not all_variables:
-        raise click.BadArgumentUsage('No variables specified')
-
+@click.option('--only', 'variables', default=None, help='Set CRS only for the specified variables')
+def set_crs(filename, proj4, variables):
     try:
         proj = Proj(proj4)
     except RuntimeError:
         raise click.BadArgumentUsage('Invalid projection: ' + proj4)
 
     with Dataset(filename, 'a') as ds:
-        variables_li = []
-        if all_variables:
-            variables_li = list(ds.variables.keys())
-        elif variables:
+        if not variables:
+            variables_li = [v for v in ds.variables if v not in ds.dimensions]
+        else:
             variables_li = [x.strip() for x in variables.split(',')]
             bad_variables = set(variables_li).difference(ds.variables.keys())
             if bad_variables:
