6098847a2693d4b319b183135c6df59a98f4f040
Brendan C. Ward <bcward@astutespruce.com>
2020-10-12 15:30:14 -0700

    Bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='trefoil',
-    version='0.3.1.1',
+    version='0.3.2',
     packages=['trefoil',
               'trefoil.analysis', 'trefoil.cli',
               'trefoil.geometry', 'trefoil.geometry.tests',
