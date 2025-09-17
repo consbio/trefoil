6427e6f55ded0ac9da76e805124b63e67bc078ac
Nikolas Stevenson-Molnar <nik.molnar@consbio.org>
2025-09-16 18:09:07 -0700

    Version 0.4.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='trefoil',
-    version='0.3.2',
+    version='0.4.1',
     packages=['trefoil',
               'trefoil.analysis', 'trefoil.cli',
               'trefoil.geometry', 'trefoil.geometry.tests',
